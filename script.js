f77b4986938604f2f73b2ccbba0774edb862594a
Jeremy Encalada <encaladajeremy@gmail.com>
2024-12-02 08:28:46 -0500

    Agregar funcionalidad de capas interactivas y mejorar estilos de navegación en el mapa

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
 /* eslint-disable no-undef */
 // Crear el mapa centrado en Loja, Ecuador
+const $navLayers = document.querySelector('#layers')
+const layers = []
+let canDrop = false
+let draggingLi = null
+
+function dragoverHandler (ev) {
+  ev.preventDefault()
+
+  if (draggingLi === ev.target) {
+    canDrop = false
+  } else {
+    ev.dataTransfer.dropEffect = 'move'
+    canDrop = true
+    // // Colocar una vista previa del elemento en donde se va a soltar
+    // if (ev.target.tagName.toLowerCase() === 'li') {
+    //   ev.target.parentElement.insertBefore(draggingLi, ev.target)
+    // } else {
+    //   $navLayers.appendChild(draggingLi)
+    // }
+  }
+}
+
+function dropHandler (ev) {
+  ev.preventDefault()
+  const data = ev.dataTransfer.getData('text/plain')
+  const li = document.querySelector(`#${data}`).parentElement
+  console.log(ev.target.nextSibling)
+  const nextLi = ev.target.nextSibling
+
+  if (ev.target.tagName.toLowerCase() === 'li' && canDrop) {
+    console.log(ev.target.nextSibling)
+    $navLayers.removeChild(li)
+
+    if (nextLi === draggingLi) {
+      console.log('inserting before', ev.target)
+      $navLayers.insertBefore(li, ev.target)
+    } else {
+      console.log('appending', ev.target)
+      $navLayers.insertBefore(li, ev.target.nextSibling)
+    }
+
+    if (!nextLi) {
+      console.log('appending at the end', ev.target)
+      $navLayers.appendChild(li)
+      return
+    }
+  }
+  draggingLi = null
+  canDrop = false
+  li.style.opacity = 1
+}
+
+$navLayers.addEventListener('dragover', dragoverHandler)
+$navLayers.addEventListener('drop', dropHandler)
+
 const map = L.map('map').setView([-3.99313, -79.20422], 13) // Coordenadas y zoom inicial
 
 // Agregar un mapa base (OpenStreetMap)
@@ -8,12 +63,53 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map)
 
+function addLayerOption (layer, opciones) {
+  const layerAux = L.geoJSON(layer, opciones)
+  const checkbox = document.createElement('input')
+  const li = document.createElement('li')
+  checkbox.type = 'checkbox'
+  checkbox.checked = false
+  checkbox.id = 'layer-' + layerAux._leaflet_id
+  checkbox.onchange = function () {
+    if (checkbox.checked) {
+      if (layers.find(l => ('layer-' + l._leaflet_id) === checkbox.id)) return
+      console.log('adding layer', layerAux)
+      layers.push(layerAux)
+      layerAux.addTo(map)
+    } else {
+      const layerAux = layers.find(l => 'layer-' + l._leaflet_id === checkbox.id)
+      console.log('removing layer', layerAux, layers)
+      if (map.hasLayer(layerAux)) {
+        map.removeLayer(layerAux)
+        layers.splice(layers.indexOf(layerAux), 1)
+      }
+    }
+  }
+
+  li.innerHTML = layer.name
+  li.draggable = true
+  li.addEventListener('dragstart', function (e) {
+    draggingLi = li
+    e.dataTransfer.setData('text/plain', checkbox.id)
+    e.dataTransfer.effectAllowed = 'move'
+    li.style.opacity = 0.5
+    e.dataTransfer.setDragImage(li, 0, 0)
+  })
+
+  li.addEventListener('dragend', function (e) {
+    draggingLi = null
+    li.style.opacity = 1
+  })
+  li.appendChild(checkbox)
+  $navLayers.appendChild(li)
+}
+
 // Función para cargar capas GeoJSON
 function cargarCapa (archivo, opciones = {}) {
   fetch(archivo)
     .then(response => response.json())
     .then(data => {
-      L.geoJSON(data, opciones).addTo(map)
+      addLayerOption(data, opciones)
     })
     .catch(err => console.error(`Error al cargar ${archivo}:`, err))
 }
@@ -26,20 +122,18 @@ cargarCapa('geojson/DIVISIONBARRIAL/DivisionBarrialHatch.geojson', {
     fillOpacity: 0.5
   }),
   onEachFeature: (feature, layer) => {
-    console.log(feature, layer)
+    // console.log(feature, layer)
     layer.bindTooltip(feature.properties.Nombres, { permanent: false, direction: 'left' })
-    // Resaltar al pasar el ratón
     layer.on('mouseover', function () {
       layer.setStyle({
-        fillOpacity: 1 // Incrementar opacidad
+        fillOpacity: 1
       })
     })
 
-    // Restaurar estilo original al salir
     layer.on('mouseout', function () {
       layer.setStyle({
-        fillOpacity: 0.5, // Restaurar opacidad original
-        color: feature.properties.color // Restaurar color original
+        fillOpacity: 0.5,
+        color: feature.properties.color
       })
     })
   }
